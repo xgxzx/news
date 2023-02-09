b5a3e32095ceb1e9c831035bb96a2131752f24ef
Vad <vadshimo@yandex.com>
2023-02-09 22:50:14 +0300

    D6.7

--- newssj/management/commands/runapscheduler.py
+++ newssj/management/commands/runapscheduler.py
@@ -65,7 +65,7 @@ class Command(BaseCommand):
 
         scheduler.add_job(
             my_job,
-            trigger=CronTrigger(second="*/10"),  # Every 10 seconds
+            trigger=CronTrigger(day_of_week="fri", hour="18", minute="00"),  # Every 10 seconds
             id="my_job",  # The `id` assigned to each job MUST be unique
             max_instances=1,
             replace_existing=True,
