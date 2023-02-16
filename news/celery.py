63100c85df6927453ec47df39abc3d014b3ba141
Vad <vadshimo@yandex.com>
2023-02-16 23:42:22 +0300

    D7.5

--- news/celery.py
+++ news/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'news.settings')
 
@@ -7,3 +8,10 @@ app = Celery('news')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'send_email_every_monday_8am': {
+        'task': 'action',
+        'schedule': crontab(hour=8, minute=0, day_of_week='monday'),
+    },
+}
--- newssj/tasks.py
+++ newssj/tasks.py
@@ -1,14 +1,51 @@
+import datetime
 from celery import shared_task
-import time
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from news import settings
+from newssj.models import *
 
 
 @shared_task
-def hello():
-    time.sleep(1)
-    print("Hello, world!")
+def send_notifications(preview, pk, title, subscribers):
+    html_content = render_to_string(
+        'post_created_email.html',
+        {
+            'text': preview,
+            'link': f'{settings.SITE_URL}/news/{pk}',
+        }
+    )
+
+    msg = EmailMultiAlternatives(
+        subject=title,
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
 
 
 @shared_task
 def weekly_notifications():
-
-    pass
+    today = datetime.datetime.now()
+    last_week = today - datetime.timedelta(days=7)
+    posts = Post.objects.filter(time_in__gte=last_week)
+    categories = set(posts.values_list('category__name', flat=True))
+    subscribers = set(Subscriber.objects.filter(category__name__in=categories).values_list('user__email', flat=True))
+    html_content = render_to_string(
+        'daily_post.html',
+        {
+            'link': settings.SITE_URL,
+            'posts': posts,
+        }
+    )
+    msg = EmailMultiAlternatives(
+        subject='Посты за неделю',
+        body='',
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        to=subscribers,
+    )
+    msg.attach_alternative(html_content, 'text/html')
+    msg.send()
